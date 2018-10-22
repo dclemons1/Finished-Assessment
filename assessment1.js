6ac830cd0673abd830261f2021ff13aadf0935d1
COR Students <corstudents@Matts-MBP-48.localdomain>
2018-10-22 12:16:17 -0400

    Finished Assessment

---
+++ assessment1.js
@@ -0,0 +1,56 @@
+///number 1
+
+/*let string = ""
+let bool = true
+let int = 1
+let float = 3.14*/
+
+///number 2
+/*let trueOrFalse = true
+if(trueOrFalse===false){
+    console.log ("1")
+} else{
+    console.log ("2");
+}*/
+
+////number 3
+/*for(i=0;i<=100;i+=2){
+    console.log(i)
+}*/
+
+////number 4
+/*let name = true
+let counter = 0
+while(name===true){
+    counter++
+    if(counter <= 20){
+        console.log("hello")
+    } else {
+        console.log("false")
+        name=false
+    }
+
+
+}*/
+
+/////number 5
+/*let trueOrFalse = true
+let FalseOrTrue = 0
+function number(value){
+    if(value===true){
+        console.log("true")
+    }
+    else {
+        console.log("false")
+    }
+}
+number(FalseOrTrue)*/
+
+///Bonus 
+/*
+D.R.Y = Don't Repeat Yourself
+*/
+
+
+
+
